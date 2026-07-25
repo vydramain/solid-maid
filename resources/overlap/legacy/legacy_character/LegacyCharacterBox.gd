1ce521d5e842e37872fd087ef396707d6f460728
Andrey Vydra <69751912+vydramain@users.noreply.github.com>
2026-07-26 01:51:24 +0600

    chore: Repository cleaned

--- resources/overlap/legacy/legacy_character/LegacyCharacterBox.gd
+++
@@ -1,5 +0,0 @@
-extends CharacterBody2D
-
-@export var speed: float = 30.
-@export var accel: float = 125.      # how fast you reach top speed
-@export var friction: float = 600.   # how fast you stop sliding
